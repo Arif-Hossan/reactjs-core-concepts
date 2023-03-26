f4cd378a3d8d7b805d8a9320c4e68d894a42f89d
Arif Hossan <arif.hossan.eu@gmail.com>
2023-03-27 00:02:28 +0600

    reactjs fundamental

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 777;
+const singer = {name : "Dr Mahfuz", job:"singer"}
+// styling
+const singerStyle = {
+  color:"Purple",
+  backgroundColor:"White"
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude, How are you?</h3>
+        </div>
+        <div className="music">
+          <p style={{color:"red",backgroundColor:"White"}}>Num : {number} </p>
+          <p style={singerStyle}>Singer : {singer.name} , {singer.job}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
